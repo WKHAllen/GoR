d3229103a4163e431257fdb56d5b2ff4bca95539
WKHAllen <wkhallen@gmail.com>
2019-11-02 10:23:06 -0500

    Add mini-pages

--- src/public/main.js
+++ src/public/main.js
@@ -1,19 +1,38 @@
+var canvas;
 var trash;
 var bin;
 
+var currentPage = 1;
+
+// button =  createButton('click me');
+// button.addClass('btn btn-primary');
+// button.position(19, 19);
+// button.mousePressed(someFunction);
+
 function setup() {
-    createCanvas(800, 600);
+    canvas = createCanvas(windowWidth, windowHeight);
+    canvas.parent('page-2');
     trash = new Trash();
     bin = new Bin(trashNames[0], width - 50, 10, 40, 40);
 }
 
 function draw() {
     background(220);
+    bin.draw();
     if (trash.moveToward(bin)) {
-        bin.draw();
         trash.draw();
     } else {
         // now trash is in the bin
         console.log('reached the bin!');
     }
+}
+
+function windowResized() {
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setPage(pageNumber) {
+    document.getElementById(`page-${currentPage}`).classList.add('hidden');
+    document.getElementById(`page-${pageNumber}`).classList.remove('hidden');
+    currentPage = pageNumber;
 }
