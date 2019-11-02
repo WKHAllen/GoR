73f0c4315d4b8ce20fe85b769424376096b2995a
WKHAllen <wkhallen@gmail.com>
2019-11-02 14:08:38 -0500

    Add click and drag functionality

--- src/public/main.js
+++ src/public/main.js
@@ -1,38 +1,82 @@
 var canvas;
-var trash;
-var bin;
+var trash = [];
+var bins = [];
 
 var currentPage = 1;
 
-// button =  createButton('click me');
-// button.addClass('btn btn-primary');
-// button.position(19, 19);
-// button.mousePressed(someFunction);
+var animating = null;
+var mouseDownPos = null;
+var mouseUpPos = null;
+
+var trashArea;
+var binArea;
+
+var debug = false;
 
 function setup() {
     canvas = createCanvas(windowWidth, windowHeight);
     canvas.parent('page-2');
-    trash = new Trash();
-    bin = new Bin(trashNames[0], width - 50, 10, 40, 40);
+    trashArea = [windowWidth / 2 - 25, windowHeight - 50, 50, 50];
+    binArea = [0, 0, windowWidth, 100];
+    initGame();
+    setPage(2); // TODO: remove this later
 }
 
 function draw() {
     background(220);
-    bin.draw();
-    if (trash.moveToward(bin)) {
-        trash.draw();
-    } else {
-        // now trash is in the bin
-        console.log('reached the bin!');
+    if (debug) {
+        fill(0, 255, 0);
+        rect(...trashArea);
+        fill(0, 0, 255);
+        rect(...binArea);
     }
+    fill(255);
+    // TODO: game logic
+    if (trash.length > 0) trash[trash.length - 1].draw();
+    for (var bin of bins)
+        bin.draw();
 }
 
 function windowResized() {
     resizeCanvas(windowWidth, windowHeight);
 }
 
+function mousePressed() {
+    if (!animating)
+        mouseDownPos = [mouseX, mouseY];
+    else
+        mouseDownPos = null;
+}
+
+function mouseReleased() {
+    if (!animating && mouseDownPos !== null) {
+        mouseUpPos = [mouseX, mouseY];
+        beginAnimation(mouseDownPos, mouseUpPos);
+    } else {
+        mouseUpPos = null;
+    }
+}
+
 function setPage(pageNumber) {
     document.getElementById(`page-${currentPage}`).classList.add('hidden');
     document.getElementById(`page-${pageNumber}`).classList.remove('hidden');
     currentPage = pageNumber;
+    if (pageNumber === 2)
+        initGame();
+}
+
+function initGame() {
+    trash = [];
+    trash.push(new Trash()); // TODO: push more trash (how much?)
+    bins = [];
+    bins.push(new Bin(trashNames[0], width - 50, 10, 40, 40)); // TODO: push 5 bins total
+}
+
+function beginAnimation(downPos, upPos) {
+    if (pointInArea(downPos, trashArea) && pointInArea(upPos, binArea))
+        animating = Math.floor(upPos[0] / (windowWidth / bins.length));
+}
+
+function pointInArea(point, area) {
+    return area[0] <= point[0] && point[0] < area[0] + area[2] && area[1] <= point[1] && point[1] < area[1] + area[3];
 }
